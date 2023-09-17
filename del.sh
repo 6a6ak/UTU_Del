aadda0a6de5fc5967ab9e40a2493d8fafeeb23f5
6a6ak <babak.ahari@gmail.com>
2023-09-17 02:41:48 +0200

    ask for del red background

--- del.sh
+++ del.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# ANSI escape codes for colored output
+RED_BG="\033[41m"
+BLACK_TEXT="\033[30m"
+RESET="\033[0m"
+
 # Default Trash folder in Ubuntu
 TRASH_DIR="$HOME/.local/share/Trash/files"
 
 # Check for a single argument
 if [ "$#" -ne 1 ]; then
-  echo "Usage: del filename"
+  echo -e "${RED_BG}${BLACK_TEXT}Usage: del filename${RESET}"
   exit 1
 fi
 
@@ -20,6 +25,14 @@ if [ -e "$dest" ]; then
   dest="$TRASH_DIR/${filename_noext}_${timestamp}.${extension}"
 fi
 
+# Ask for user confirmation
+echo -e "${RED_BG}${BLACK_TEXT}Are you sure you want to move $1 to Trash? [y/N]${RESET}"
+read -r answer
+
+if [ "$answer" != "Y" ] && [ "$answer" != "y" ]; then
+  echo "Operation cancelled."
+  exit 1
+fi
+
 # Move the file to the Trash folder
-mv "$1" "$dest"
-echo "Moved $1 to $dest"
+mv "$1" "$dest" && echo -e "${RED_BG}${BLACK_TEXT}Moved $1 to $dest${RESET}" || echo -e "${RED_BG}${BLACK_TEXT}Failed to move $1${RESET}"
